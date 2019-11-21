0a188bd364ccb9b536e15472e6a8f06858652fb5
dvs-tulva <56066024+dvs-tulva@users.noreply.github.com>
2019-11-21 17:43:36 +0700

    init commit

---
+++ js/index.js
@@ -0,0 +1,46 @@
+"use strict";
+let app = new Vue({
+    el: "#app",
+    data: {
+        message: "Hello leviathan!"
+    }
+});
+/**/
+let app2 = new Vue({
+    el: "#app-2",
+    data: {
+        message: "You loaded this page on " + (new Date()).toString()
+    }
+});
+/**/
+let whatever = new Vue({
+    el: ".whatever",
+    data: {
+        message01: "This is for .whatever class",
+        message02: "This is message 02"
+    }
+});
+/**/
+let app3 = new Vue({
+    el: "#app-3",
+    data: {
+        seen: true
+    }
+});
+/**/
+let app4 = new Vue({
+    el: "#app-4",
+    data: {
+        processes: [
+            {name: "Learn Java", didStart: true},
+            {name: "Learn Javascript", didStart: true},
+            {name: "Learn Laravel", didStart: false},
+            {name: "Learn Vuejs", didStart: true},
+            {name: "Learn Spring Boot", didStart: true},
+            {name: "Learn Golang", didStart: false},
+            {name: "Learn Spring Security", didStart: true},
+        ]
+    }
+});
+/**/
+app4.processes.push({name: "C++", didStart: false});
